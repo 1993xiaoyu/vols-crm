f6624b8784c38ab4bb565051a2e87d76f55aa828
1993xiaoyu <850466210@qq.com>
2022-11-28 19:35:18 +0800

    地图初次处理

--- src/views/cockpit/components/data-division-box.vue
+++ src/views/cockpit/components/data-division-box.vue
@@ -9,64 +9,109 @@
 <script setup>
 import { onMounted } from 'vue';
 
-const BMap = window.BMap;
 var map = null;
 
 const initMap = () => {
-    map = new BMap.Map('mapBox');
+    map = new BMapGL.Map('mapBox');
     //设置地图的中心点如合肥的坐标
-    let point = new BMap.Point(106.714476, 26.60403);
+    var point = new BMapGL.Point(116.404, 39.925);
     // 初始化地图，设置中心点坐标和地图级别
-    map.centerAndZoom(point, 8);
+    map.centerAndZoom(point, 10);
     //禁止拖拽
     map.disableDragging();
     map.disableDoubleClickZoom();
     map.disablePinchToZoom();
     map.disableKeyboard();
     map.disableScrollWheelZoom();
-
+    map.setTilt(50);
     getBoundary();
 };
 
 const getBoundary = () => {
-    const bdary = new BMap.Boundary();
-    bdary.get('贵州省', (rs) => {
-        const count = rs.boundaries.length;
+    var bd = new BMapGL.Boundary();
+    bd.get('北京市', function (rs) {
+        var count = rs.boundaries.length; //行政区域的点有多少个
         if (count === 0) {
             return;
         }
-        const EN_JW = '180, 90;';
-        const NW_JW = '-180,  90;';
-        const WS_JW = '-180, -90;';
-        const SE_JW = '180, -90;';
-        // 东南西北四个角添加一个覆盖物
-        const ply1 = new BMap.Polygon(
-            rs.boundaries[0] + SE_JW + SE_JW + WS_JW + NW_JW + EN_JW + SE_JW,
-            {
-                strokeColor: 'none',
-                fillColor: '#fff',
-                fillOpacity: 1,
-                strokeOpacity: 1,
-            }
-        );
-        map.addOverlay(ply1);
-        var pointArray = [];
-        // 绘制‘贵州省’整体的外轮廓
-        for (let i = 0; i < count; i++) {
-            const ply = new BMap.Polygon(rs.boundaries[i], {
-                strokeWeight: 0.5,
-                strokeColor: '#006BE9',
-                fillColor: '#006BE9',
-            });
-            map.addOverlay(ply);
-            pointArray = pointArray.concat(ply.getPath());
-        }
 
-        map.setViewport(pointArray, {
-            enableAnimation: false,
-        }); // 调整视野
-        getRegion();
+        // const EN_JW = '180, 90;';
+        // const NW_JW = '-180,  90;';
+        // const WS_JW = '-180, -90;';
+        // const SE_JW = '180, -90;';
+        // 东南西北四个角添加一个覆盖物
+        // const ply1 = new BMapGL.Polygon(
+        //     rs.boundaries[0] + SE_JW + SE_JW + WS_JW + NW_JW + EN_JW + SE_JW,
+        //     5000,
+        //     {
+        //         strokeColor: 'none',
+        //         fillColor: '#fff',
+        //         fillOpacity: 1,
+        //         strokeOpacity: 1,
+        //     }
+        // );
+        // map.addOverlay(ply1);
+
+        var pointArray = [];
+        for (var i = 0; i < count; i++) {
+            var path = [];
+            var str = rs.boundaries[i].replace(' ', '');
+            var points = str.split(';');
+            for (var j = 0; j < points.length; j++) {
+                var lng = points[j].split(',')[0];
+                var lat = points[j].split(',')[1];
+                path.push(new BMapGL.Point(lng, lat));
+            }
+            var prism = new BMapGL.Prism(path, 5000, {
+                topFillColor: '#5679ea',
+                topFillOpacity: 0.5,
+                sideFillColor: '#5679ea',
+                sideFillOpacity: 0.9,
+            });
+            map.addOverlay(prism);
+            pointArray = pointArray.concat(prism.getPath());
+        }
     });
+
+    // const bdary = new BMapGL.Boundary();
+    // bdary.get('贵州省', (rs) => {
+    //     const count = rs.boundaries.length;
+    //     if (count === 0) {
+    //         return;
+    //     }
+    //     const EN_JW = '180, 90;';
+    //     const NW_JW = '-180,  90;';
+    //     const WS_JW = '-180, -90;';
+    //     const SE_JW = '180, -90;';
+    //     // 东南西北四个角添加一个覆盖物
+    //     const ply1 = new BMapGL.Prism(
+    //         rs.boundaries[0] + SE_JW + SE_JW + WS_JW + NW_JW + EN_JW + SE_JW,
+    //         5000,
+    //         {
+    //             strokeColor: 'none',
+    //             fillColor: '#fff',
+    //             fillOpacity: 1,
+    //             strokeOpacity: 1,
+    //         }
+    //     );
+    //     map.addOverlay(ply1);
+    //     var pointArray = [];
+    //     // 绘制‘贵州省’整体的外轮廓
+    //     for (let i = 0; i < count; i++) {
+    //         const ply = new BMapGL.Prism(rs.boundaries[i], 5000, {
+    //             strokeWeight: 0.5,
+    //             strokeColor: '#006BE9',
+    //             fillColor: '#006BE9',
+    //         });
+    //         map.addOverlay(ply);
+    //         pointArray = pointArray.concat(ply.getPath());
+    //     }
+
+    //     map.setViewport(pointArray, {
+    //         enableAnimation: false,
+    //     }); // 调整视野
+    //     // getRegion();
+    // });
 };
 
 var dataArr = [
@@ -110,11 +155,11 @@ var dataArr = [
 
 const getRegion = () => {
     dataArr.forEach((element) => {
-        var bdary = new BMap.Boundary();
+        var bdary = new BMapGL.Boundary();
         bdary.get(element['name'], (rs) => {
             var count = rs.boundaries.length;
             for (let i = 0; i < count; i++) {
-                var ply = new BMap.Polygon(rs.boundaries[i], {
+                var ply = new BMapGL.Polygon(rs.boundaries[i], {
                     strokeWeight: 0.5,
                     strokeColor: '#fff',
                     fillOpacity: 0.6,
@@ -123,7 +168,7 @@ const getRegion = () => {
                 map.addOverlay(ply);
             }
             citySetLabel(
-                new BMap.Point(element['cp'][0], element['cp'][1]),
+                new BMapGL.Point(element['cp'][0], element['cp'][1]),
                 element['name']
             );
         });
@@ -131,8 +176,8 @@ const getRegion = () => {
 };
 
 const citySetLabel = (cityCenter, cityName) => {
-    var label = new BMap.Label(cityName, {
-        offset: new BMap.Size(-20, -10),
+    var label = new BMapGL.Label(cityName, {
+        offset: new BMapGL.Size(-20, -10),
         position: cityCenter,
     });
     label.setStyle({
--- src/views/cockpit/components/scene-box.vue
+++ src/views/cockpit/components/scene-box.vue
@@ -16,13 +16,13 @@ import { onMounted } from 'vue';
 import sceneBg from '../assets/scene-bg.png';
 var map = null;
 const initMap = () => {
-    map = new BMap.Map('sceneRoute');
+    map = new BMapGL.Map('sceneRoute');
     //设置地图的中心点如合肥的坐标
-    let point = new BMap.Point(116.404, 39.915);
-    var p1 = new BMap.Point(116.301934, 39.977552);
-    var p2 = new BMap.Point(116.508328, 39.919141);
+    let point = new BMapGL.Point(116.404, 39.915);
+    var p1 = new BMapGL.Point(116.301934, 39.977552);
+    var p2 = new BMapGL.Point(116.508328, 39.919141);
 
-    var driving = new BMap.DrivingRoute(map, {
+    var driving = new BMapGL.DrivingRoute(map, {
         renderOptions: { map: map, autoViewport: true },
     });
     driving.search(p1, p2);
